2bb58446d650efcbe4bc0b8bfac466012f20d913
fzd5bqjpvp-source <fzd5bqjpvp@privaterelay.appleid.com>
2026-05-30 05:39:37 +0000

    Add files via upload

---
+++ RNKA.py
@@ -0,0 +1,32 @@
+
+# File: RNKO.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Richmond Nana Kofi Agyarko
+# STUDENT ID: 2425403113
+# Email: 2425403113@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Richmond Nana Kofi Agyarko this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+print()
+
+print("...RNKO")
+
+print()
+
+print("...RRRRRRRRRRR.........NNNN....NNNN........KKKK....KKKK..........AAAAAAAA.......")
+print("...RRR....RRRR.........NNNNN...NNNN........KKKK...KKKK..........AAAAAAAAAA......")
+print("...RRR...RRRR..........NNNNN...NNNN........KKKK..KKKK..........AAA......AAA.....")
+print("...RRR..RRRR...........NNNNNN..NNNN....... KKKK.KKK............AAA......AAA.....")
+print("...RRR.RRRR............NNNNNNN.NNNN........KKKK.KK.............AAA......AAA.....")
+print("...RRR...RRRR..........NNNN.NNNNNNN........KKKK.KKKK...........AAAAAAAAAAAA.....")
+print("...RRR....RRRR.........NNNN..NNNNNN........KKKK..KKKK..........AAAAAAAAAAAA.....")
+print("...RRR.....RRRR........NNNN...NNNNN........KKKK...KKKK.........AAA......AAA.....")
+print("...RRR.....RRRR...**...NNNN...NNNNN...**...KKKK....KKKK...**...AAA......AAA...**")
+print("...RRR.....RRRR...**...NNNN....NNNN...**...KKKK....KKKK...**...AAA......AAA...**")
+
+
+print()
