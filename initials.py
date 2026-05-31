26abd0f9b8d6fbc4bba5c4be8abdcd7c0d9f7d86
fzd5bqjpvp-source <fzd5bqjpvp@privaterelay.appleid.com>
2026-05-31 05:49:10 +0000

    Implement initials printing functionality
    Added a program to print out initials with details.

--- initials.py
+++ initials.py
@@ -1,6 +1,36 @@
 
 def main():
-  pass #code goes here
+  # File: RNKA.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Richmond Nana Kofi Agyarko
+# STUDENT ID: 2425403113
+# Email: 2425403113@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Richmond Nana Kofi Agyarko this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+print()
+
+print("...RNKA")
+
+print()
+
+print("...RRRRRRRRRRR.........NNNN....NNNN........KKKK....KKKK.....")
+print("...RRR....RRRR.........NNNNN...NNNN........KKKK...KKKK......")
+print("...RRR...RRRR..........NNNNN...NNNN........KKKK..KKKK.......")
+print("...RRR..RRRR...........NNNNNN..NNNN....... KKKK.KKK.........")
+print("...RRR.RRRR............NNNNNNN.NNNN........KKKK.KK..........")
+print("...RRR...RRRR..........NNNN.NNNNNNN........KKKK.KKKK........")
+print("...RRR....RRRR.........NNNN..NNNNNN........KKKK..KKKK.......")
+print("...RRR.....RRRR........NNNN...NNNNN........KKKK...KKKK......")
+print("...RRR.....RRRR...**...NNNN...NNNNN...**...KKKK....KKKK...**")
+print("...RRR.....RRRR...**...NNNN....NNNN...**...KKKK....KKKK...**")
+
+
+print()
 
 
 
